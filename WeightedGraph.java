253cb59cb47e0f125290475315201098e4bb3550
Tyler Fleetwood <tyfleetwood@live.com>
2018-04-09 23:33:36 -0600

    Update WeightedGraph.java
    Added ClosestPair and formatted the output to be step-by-step. For some reason on my computer, the supposedly infinity symbol is replaced by "?" maybe yours will do it correctly?

--- WeightedGraph.java
+++ WeightedGraph.java
@@ -17,7 +17,11 @@ public class WeightedGraph {
 		// we're going to set all adjacenies to infinity for now
 		for (int i = 0; i < numv; i++) {
 			for (int j = 0; j < numv; j++) {
-				matrix[i][j] = INFINITY;
+				if(i == j){
+					matrix[i][j] = 0;
+				}else{
+					matrix[i][j] = INFINITY;
+				}
 			}
 		}
 	}
@@ -36,14 +40,29 @@ public class WeightedGraph {
 	}
 
 	public void displayMatrix() {
+		
+		System.out.print("    ");
+		
+		for(int i = 0; i < vertices.length; i++){
+			System.out.format("%-4s", vertices[i].getLabel());
+		}
+		System.out.println();
+		
 		for (int i = 0; i < matrix.length; i++) {
 			//printVertex(i);
-			System.out.print(" ");
+			System.out.format("%-4s", vertices[i].getLabel());
 			for (int j = 0; j < matrix.length; j++) {
-				System.out.print(matrix[i][j] + " ");
+				if(matrix[i][j] != INFINITY){
+					System.out.format("%-4s", matrix[i][j]);
+
+				}else{
+					System.out.format("%-4s" ,"\u221e");
+				}
+
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
 
 	public void Prims() {
@@ -125,6 +144,33 @@ public class WeightedGraph {
 		}
 		System.out.println();
 	}
+	
+/*	public void PrintMatrix(){
+		
+		System.out.println("  " + );
+		
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix.length; j++){
+				
+			}
+		}
+	}*/
+	
+	public void ClosestPair(){
+		
+		for(int k = 0; k < matrix.length; k++){
+			for(int i = 0; i < matrix.length; i++){
+				for(int j = 0; j < matrix.length; j++){
+					if(matrix[i][k] == INFINITY || matrix[j][k] == INFINITY){
+					}else if(matrix[i][j] >  matrix[i][k] + matrix [k][j]){
+						matrix[i][j] =  matrix[i][k] + matrix [k][j];
+						
+						displayMatrix();
+						}
+					}
+			}
+		}
+	}
 
 	public static void main(String args[]) {
 		WeightedGraph graph = new WeightedGraph(10);
@@ -157,6 +203,7 @@ public class WeightedGraph {
 
 		graph.Prims();
 		graph.Kruskals();
+		graph.ClosestPair();
 
 	}
 }
